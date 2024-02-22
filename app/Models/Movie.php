42cd54a377b7eb6543f51f0bd372413873d1e732
KiberGod <stupakov.kirill13@gmail.com>
2024-02-22 09:36:14 +0200

    Added models

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Movie extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'publication_status', 'poster'];
+
+    public function genres()
+    {
+        return $this->belongsToMany(Genre::class, 'genre_movie','movie_id', 'genre_id');
+    }
 }
